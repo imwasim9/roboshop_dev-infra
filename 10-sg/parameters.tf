c93217f52876a34fd357e58ead61c78ee6ceecfa
Wasim <imwasim.shaik@gmail.com>
2026-02-02 06:41:02 +0530

    commit m

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "sg_id"{
     count = length(var.sg_names)
     name = "/${var.project_name}/${var.environment}/${var.sg_names[count.index]}_sg_id"
-    type = "String"
+    type = "String" # type value should be in quotes here
     value = module.sg[count.index].sg_id
 }
