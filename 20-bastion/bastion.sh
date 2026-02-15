009a697c7d78b6ffc7d5d9514c537931e29fd013
Wasim <imwasim.shaik@gmail.com>
2026-02-15 09:06:25 +0530

    commit

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo yum install -y yum utils
+sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.release.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
