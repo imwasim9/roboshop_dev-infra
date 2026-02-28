d44d7fedb0c31d98f9358237ed2ae1f83c15c18f
Wasim <imwasim.shaik@gmail.com>
2026-02-28 20:16:03 +0530

    created vpc s3 bucket for storing remote state

---
+++ 80-frontend-alb/main.tf
@@ -0,0 +1,45 @@
+resource "aws_lb" "frontend-alb" {
+  name               = "${local.common_name_suffix}-frontend-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [local.frontend_alb_sg_id]
+  subnets            = local.public_subnet_ids
+
+  enable_deletion_protection = false # prevents accidental deletion from UI/terraform
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-frontend-alb"
+    }
+  )
+}
+
+resource "aws_lb_listener" "frontend_alb" {
+  load_balancer_arn = aws_lb.frontend-alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/html"
+      message_body = "Hi, I am from frontend alb HTTPS"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "frontend_alb" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "roboshop-${var.environment}.${var.domain_name}"
+  type    = "A"
+  allow_overwrite = true
+
+  alias {
+    # These are ALB details, not our domain details
+    name                   = aws_lb.frontend-alb.dns_name
+    zone_id                = aws_lb.frontend-alb.zone_id
+    evaluate_target_health = true
+  }
+}
---
+++ 80-frontend-alb/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "remote-state-wasdaws.shop"
+    key          = "roboshop-dev-frontend_alb"
+    use_lockfile = true
+    region = "us-east-1"
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  
+}
