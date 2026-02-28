e67a83486a50acbae2e3a9ef98a48f64c822dbcb
Wasim <imwasim.shaik@gmail.com>
2026-02-28 20:18:45 +0530

    created vpc s3 bucket for storing remote state

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -19,6 +19,8 @@ resource "aws_lb_listener" "frontend_alb" {
   load_balancer_arn = aws_lb.frontend-alb.arn
   port              = "443"
   protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
+  certificate_arn =  local.frontend_alb_certificate_arn
 
   default_action {
     type = "fixed-response"
