bd9921da81fd89c8a32fc49c617852a076106a12
Wasim <imwasim.shaik@gmail.com>
2026-03-12 06:31:50 +0530

    silly autoscaling_group.main.name mistake caused error

--- 80-frontend-alb/locals.tf
+++ 80-frontend-alb/locals.tf
@@ -4,6 +4,7 @@ locals {
     frontend_alb_sg_id = data.aws_ssm_parameter.frontend-alb_sg_id.value
     frontend_alb_certificate_arn = data.aws_ssm_parameter.frontend_alb_certificate_arn.value
     public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+    zone_id = data.aws_route53_zone.zone.zone_id
     common_tags ={
         Project = var.project_name
         Environment = var.environment
--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "frontend-alb" {
+resource "aws_lb" "frontend_alb" {
   name               = "${local.common_name_suffix}-frontend-alb"
   internal           = false
   load_balancer_type = "application"
@@ -16,7 +16,7 @@ resource "aws_lb" "frontend-alb" {
 }
 
 resource "aws_lb_listener" "frontend_alb" {
-  load_balancer_arn = aws_lb.frontend-alb.arn
+  load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
@@ -26,22 +26,22 @@ resource "aws_lb_listener" "frontend_alb" {
     type = "fixed-response"
     fixed_response {
       content_type = "text/html"
-      message_body = "Hi, I am from frontend alb HTTPS"
+      message_body = "<h1>Hi, I am from HTTPS frontend ALB</h1>"
       status_code  = "200"
     }
   }
 }
 
 resource "aws_route53_record" "frontend_alb" {
-  zone_id = data.aws_route53_zone.zone.zone_id
+  zone_id = local.zone_id
   name    = "roboshop-${var.environment}.${var.domain_name}"
   type    = "A"
   allow_overwrite = true
 
   alias {
     # These are ALB details, not our domain details
-    name                   = aws_lb.frontend-alb.dns_name
-    zone_id                = aws_lb.frontend-alb.zone_id
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
     evaluate_target_health = true
   }
 }
