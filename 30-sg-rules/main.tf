5fcd43c73ea0923754f9d79a75c71ec2036292b3
Wasim <imwasim.shaik@gmail.com>
2026-03-23 08:05:53 +0530

    sg rule for cart_backend-alb is incorrect i.e source

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -127,8 +127,8 @@ resource "aws_security_group_rule" "cart_bastion" {
 
 resource "aws_security_group_rule" "cart_backend_alb" {
   type              = "ingress"
-  security_group_id = local.backend_alb_sg_id
-  source_security_group_id = local.cart_sg_id
+  security_group_id = local.cart_sg_id
+  source_security_group_id =  local.backend_alb_sg_id
   from_port         = 8080
   protocol          = "tcp"
   to_port           = 8080
