a078f274c45fb23b0a633e0de21ee3da57aff833
Wasim <imwasim.shaik@gmail.com>
2026-03-27 08:06:09 +0530

    added cdn for roboshop /media/*

--- 91-cloudfront/data.tf
+++ 91-cloudfront/data.tf
@@ -9,3 +9,8 @@ data "aws_cloudfront_cache_policy" "cachingDisabled" {
 data "aws_ssm_parameter" "certificate_arn" {
     name = "/${var.project_name}/${var.environment}/frontend_alb_certificate_arn"
 }
+
+data "aws_route53_zone" "zone"{
+    name = var.domain_name
+    private_zone = false
+}
--- 91-cloudfront/main.tf
+++ 91-cloudfront/main.tf
@@ -22,9 +22,41 @@ resource "aws_cloudfront_distribution" "roboshop" {
     ordered_cache_behavior {
       path_pattern = "/media/*"
       allowed_methods = ["GET", "HEAD", "OPTIONS"]
-      cache_methods = ["GET", "HEAD", "OPTIONS"]
-      target_origin_id = "${var.project_name}-${var.enviroment}.${var.domain_name}"
+      cached_methods = ["GET", "HEAD", "OPTIONS"]
+      target_origin_id = "${var.project_name}-${var.environment}.${var.domain_name}"
       viewer_protocol_policy = "https-only"
       cache_policy_id = local.cachingOptimised
     }
+
+    price_class = "PriceClass_All"
+    restrictions {
+      geo_restriction {
+        restriction_type = "whitelist"
+        locations = ["US", "IN","TH", "DE"]
+      }
+    }
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project_name}-${var.environment}"
+            }
+    )
+
+    viewer_certificate {
+      acm_certificate_arn = local.cdn_certificate_arn
+      ssl_support_method = "smi-only"
+    }
+}
+
+resource "aws_route53_record" "cdn" {
+  zone_id = local.zone_id
+  name = "${var.environment}.${var.domain_name}"
+  type = A
+  allow_overwrite = true
+
+  alias{
+    name = aws_cloudfront_distribution.roboshop.domain_name
+    zone_id = aws_cloudfront_distribution.roboshop.hosted_zone_id
+    evaluate_target_health = true
+  }
 }
